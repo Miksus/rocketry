9217c3bdee8fde6fb7f03cda0ade5979d8f9ffc7
Mikael Koli <koli.mikael@gmail.com>
2021-01-12 21:28:29 +0200

    test: refactored script tests

--- pypipe/test/task/script/test_run.py
+++ pypipe/test/task/script/test_run.py
@@ -2,32 +2,54 @@
 # TODO
 #import pytest
 #
-#from pypipe import Scheduler, ScriptTask
+from pypipe.task import ScriptTask
 #from pypipe.core.task.base import Task
 #
 #
 #Task.use_instance_naming = True
 from pypipe import session
+import pytest
 
-def test_success(tmpdir, successing_script_path):
-    # Going to tempdir to dump the log files there
+
+
+@pytest.mark.parametrize(
+    "script_path,expected_outcome,exc_cls",
+    [
+        pytest.param(
+            "scripts/succeeding_script.py", 
+            "success",
+            None,
+            id="Success"),
+        pytest.param(
+            "scripts/failing_script.py", 
+            "fail", 
+            RuntimeError,
+            id="Failure"),
+    ],
+)
+def test_run(tmpdir, script_files, script_path, expected_outcome, exc_cls):
     with tmpdir.as_cwd() as old_dir:
         session.reset()
         task = ScriptTask(
-            successing_script_path, 
-            execution="daily",
+            script_path, 
+            name="a task"
         )
-        task()
-        assert task.status == "success"
 
-def test_failure(tmpdir, failing_script_path):
-    # Going to tempdir to dump the log files there
-    with tmpdir.as_cwd() as old_dir:
-        session.reset()
-        task = ScriptTask(
-            failing_script_path, 
-            execution="daily",
-        )
-        with pytest.raises(ZeroDivisionError):
+        if exc_cls:
+            # Failing task
+            with pytest.raises(exc_cls):
+                task()
+        else:
+            # Succeeding task
             task()
-        assert task.status == "fail"
+
+        assert task.status == expected_outcome
+
+        df = session.get_task_log()
+        records = df[["task_name", "action"]].to_dict(orient="record")
+        assert [
+            {"task_name": "a task", "action": "run"},
+            {"task_name": "a task", "action": expected_outcome},
+        ] == records
+
+
