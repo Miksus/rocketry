284734cc371c435f46e52ad08c2c0c379d410d33
Mikael Koli <koli.mikael@gmail.com>
2021-01-15 20:32:37 +0200

    fix: if kwargs supplemented, SchedulerStarted and TasksAlive would have crashed

--- pypipe/conditions/scheduler.py
+++ pypipe/conditions/scheduler.py
@@ -14,13 +14,13 @@ def SchedulerCycles(_scheduler_, **kwargs):
     return _scheduler_.n_cycles
 
 @Statement.from_func(historical=True, quantitative=False)
-def SchedulerStarted(_scheduler_, _start_, _end_):
+def SchedulerStarted(_scheduler_, _start_, _end_, **kwargs):
     "Check whether "
     dt = _scheduler_.startup_time
     return _start_ <= dt <= _end_
 
 @Statement.from_func(historical=False, quantitative=True)
-def TasksAlive(_scheduler_):
+def TasksAlive(_scheduler_, **kwargs):
     "Check whether "
     alive_tasks = [_scheduler_.is_alive(task) for task in _scheduler_.tasks]
-    return len(alive_tasks)
+    return alive_tasks
