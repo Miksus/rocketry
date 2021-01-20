10135edaf982704880ee0ece294c661f3e869888
Mikael Koli <koli.mikael@gmail.com>
2021-01-20 19:17:38 +0200

    add: new options to PipInstall

--- atlas/task/maintain/pip.py
+++ atlas/task/maintain/pip.py
@@ -2,12 +2,26 @@
 from atlas.core.task import Task
 
 import sys
+import subprocess
 
 class PipInstall(Task):
-    def execute_action(self, interpreter=None, file=None, **kwargs):
+    def execute_action(self, 
+                        interpreter=None, 
+                        requirements=None, packages=None, 
+                        options=None,
+                        **kwargs):
+                        
         if interpreter is None:
             interpreter = sys.executable
-        if file is None:
-            file = "requirements.txt"
+        if requirements is None and packages is None:
+            requirements = "requirements.txt"
+
+        pip_install = [interpreter, "-m", "pip", "install"]
+        options = [] if options is None else options
         # https://pip.pypa.io/en/stable/reference/pip_install/#install-requirement
-        subprocess.check_call([interpreter, "-m", "pip", "install", "-r", file])
+
+        # TODO: https://stackoverflow.com/a/27254355/13696660
+        if packages is not None:
+            subprocess.check_call(pip_install + options + packages)
+        else: 
+            subprocess.check_call(pip_install + options + ["-r", requirements])
