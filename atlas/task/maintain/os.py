500d3663b7f754d208581d9e648a6eaa6fa9ee93
Mikael Koli <koli.mikael@gmail.com>
2021-01-21 22:00:34 +0200

    fix: restart now allow also soft restarting (non instant)

--- atlas/task/maintain/os.py
+++ atlas/task/maintain/os.py
@@ -1,15 +1,19 @@
 
 from atlas.core.task import Task
 
-import os, sys
+import os, sys, subprocess
 
 class Restart(Task):
     "Restart the scheduler"
 
-    def execute_action(self, argv=None, exit=True, **kwargs):
-        if argv is None:
-            argv = sys.argv
-        # Works on Windows at least:
-        os.execl(sys.executable, sys.executable, *sys.argv)
-        if exit:
+    def execute_action(self, hard=False, new_window=False, **kwargs):
+        python = sys.executable
+        if hard:
+            os.execl(python, python, *sys.argv)
+            # From now on, there is no code to execute
+        else:
+            if new_window:
+                creation_flags = subprocess.CREATE_NEW_CONSOLE
+            cmd = [python, *sys.argv]
+            subprocess.Popen(cmd, shell=False, creation_flags=creation_flags)
             sys.exit()
