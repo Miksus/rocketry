430d62df929c3c77ce1d780e3b3c6602187af9c7
Mikael Koli <koli.mikael@gmail.com>
2021-01-24 22:43:45 +0200

    test: add test for MultiScheduler with ScriptTasks

---
+++ atlas/test/schedule/multiprocess/test_script_tasks.py
@@ -0,0 +1,58 @@
+
+
+from atlas.core import MultiScheduler
+from atlas.conditions import SchedulerCycles, TaskFinished, TaskStarted, DependSuccess, AlwaysTrue
+
+from atlas.task import ScriptTask
+#from atlas.core.task.base import Task
+#
+#
+#Task.use_instance_naming = True
+from atlas import session
+import pytest
+
+@pytest.mark.parametrize(
+    "script_path,expected_outcome,exc_cls",
+    [
+        pytest.param(
+            "scripts/succeeding_script.py", 
+            "success",
+            None,
+            id="Success"),
+        pytest.param(
+            "scripts/failing_script.py", 
+            "fail", 
+            RuntimeError,
+            id="Failure"),
+    ],
+)
+def test_run(tmpdir, script_files, script_path, expected_outcome, exc_cls):
+    with tmpdir.as_cwd() as old_dir:
+        session.reset()
+        task = ScriptTask(
+            script_path, 
+            name="a task",
+            start_cond=AlwaysTrue()
+        )
+
+        scheduler = MultiScheduler(
+            [
+                task,
+            ], shut_condition=TaskStarted(task="a task") >= 3
+        )
+        scheduler()
+
+        history = task.get_history()
+        
+        if expected_outcome == "fail":
+            failures = history[history["action"] == "fail"]
+            assert 3 == len(failures)
+
+            # Check it has correct traceback in message
+            for tb in failures["message"]:
+                assert "Traceback (most recent call last):" in tb
+                assert "RuntimeError: This task failed" in tb
+        else:
+            success = history[history["action"] == "success"]
+            assert 3 == len(success)
+
