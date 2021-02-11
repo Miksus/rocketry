5a8edb864d8f9171452981294e1ba30f9bcf2c28
Mikael Koli <koli.mikael@gmail.com>
2021-02-11 21:20:41 +0200

    fix: column "task_name" is not existing for schedulers and the task adapter may be useful for reading its log files.

--- atlas/core/log/adapter.py
+++ atlas/core/log/adapter.py
@@ -58,7 +58,6 @@ class TaskAdapter(logging.LoggerAdapter):
                 return df
             elif hasattr(handler, "read"):
                 df = pd.DataFrame(handler.read())
-                df["task_name"] = df["task_name"].astype(str)
 
                 if task_name is not None:
                     df = df[df["task_name"] == task_name]
