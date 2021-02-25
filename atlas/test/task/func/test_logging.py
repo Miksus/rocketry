7d237fce2cf12a6a8f6a5938f48dfef2afbc4c87
Mikael Koli <koli.mikael@gmail.com>
2021-02-25 20:29:10 +0200

    test: new test to test the action_start in log

--- atlas/test/task/func/test_logging.py
+++ atlas/test/task/func/test_logging.py
@@ -6,6 +6,8 @@ from atlas.task import FuncTask
 from atlas.core.task.base import Task, get_task
 from atlas import session
 
+import pandas as pd
+
 def test_running(tmpdir):
     
     # Going to tempdir to dump the log files there
@@ -62,3 +64,32 @@ def test_without_handlers(tmpdir):
         )
         task()
         assert task.status is None
+
+
+@pytest.mark.parametrize("method",
+    [
+        pytest.param("log_success", id="Success"),
+        pytest.param("log_failure", id="Failure"),
+        pytest.param("log_inaction", id="Inaction"),
+        pytest.param("log_termination", id="Termination"),
+    ],
+)
+def test_action_start(tmpdir, method):
+    
+    # Going to tempdir to dump the log files there
+    with tmpdir.as_cwd() as old_dir:
+        session.reset()
+        task = FuncTask(
+            lambda : None,
+        )
+        task.log_running()
+        getattr(task, method)()
+
+        df = session.get_task_log()
+
+        # First should not have action_start
+        assert pd.isna(df["action_start"].tolist()[0])
+
+        # Second should and that should be datetime
+        assert "nan" != str(df["action_start"].tolist()[1])
+        assert "2000-01-01" <= str(df["action_start"].tolist()[1])
