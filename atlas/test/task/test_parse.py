202dbc237b8d9595e46d598fcedfab1e735be0be
Mikael Koli <koli.mikael@gmail.com>
2021-03-07 15:59:12 +0200

    test: added task test parsing tests

---
+++ atlas/test/task/test_parse.py
@@ -0,0 +1,43 @@
+
+
+from atlas.core import Scheduler
+from atlas.parse import parse_task
+from atlas.task import FuncTask, PyScript
+
+from textwrap import dedent
+from atlas import session
+
+import sys
+import pytest
+from pathlib import Path
+
+@pytest.mark.parametrize(
+    "obj,cls,attrs",
+    [
+            pytest.param(
+            {"class": "PyScript", "path": "tasks/main.py"}, 
+            PyScript,
+            {"path": Path("tasks/main.py"), "func": "main", "name": "tasks.main:main"},
+            id="PyScript defaults"
+        ),
+        pytest.param(
+            {"class": "PyScript", "path": "tasks/funcs.py", "func": "myfunc"}, 
+            PyScript,
+            {"path": Path("tasks/funcs.py"), "func": "myfunc", "name": "tasks.funcs:myfunc"},
+            id="PyScript simple"
+        ),
+        pytest.param(
+            {"class": "PyScript", "path": "tasks/funcs.py", "func": "myfunc", "name": "a task"}, 
+            PyScript,
+            {"path": Path("tasks/funcs.py"), "func": "myfunc", "name": "a task"},
+            id="PyScript with name"
+        ),
+    ]
+)
+def test_parse_attrs(tmpdir, obj, cls, attrs):
+    with tmpdir.as_cwd() as old_dir:
+        session.reset()
+        task = parse_task(obj)
+        assert isinstance(task, cls)
+        for attr, val in attrs.items():
+            assert val == getattr(task, attr)
