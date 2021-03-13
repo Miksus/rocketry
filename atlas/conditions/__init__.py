43b9aca92b76f886745610ce2385b0a27561b63d
Mikael Koli <koli.mikael@gmail.com>
2021-03-13 11:25:16 +0200

    add: missing import in __init__.py

--- atlas/conditions/__init__.py
+++ atlas/conditions/__init__.py
@@ -3,7 +3,7 @@ from .task import *
 from .scheduler import *
 from .time import *
 from .git import *
-from .parameter import ParamExists
+from .parameter import ParamExists, IsParameter
 from atlas.core.conditions import AlwaysFalse, AlwaysTrue, All, Any, Not
 
 true = AlwaysTrue()
