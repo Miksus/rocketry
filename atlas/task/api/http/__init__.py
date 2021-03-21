bc7127757169983a33009f89f03aae2bc6b8efd4
Mikael Koli <koli.mikael@gmail.com>
2021-03-21 15:54:56 +0200

    add: AddressPinger for pinging the API IP address in case of dynamic IP or to connect schedulers automatically to a web UI.

--- atlas/task/api/http/__init__.py
+++ atlas/task/api/http/__init__.py
@@ -1,1 +1,1 @@
-from .task import HTTPConnection
+from .task import HTTPConnection, AddressPinger
--- atlas/task/api/http/task.py
+++ atlas/task/api/http/task.py
@@ -9,16 +9,33 @@ from atlas.parse import parse_task
 from atlas.core import Task, Parameters
 from atlas.core.task import register_task_cls
 
+from pybox.network import get_ip
 from .routes import rest_api
 from .models import AtlasJSONEncoder
 
 from threading import Thread
 import time
+import platform
+import requests
 import pandas as pd
 
 
 @register_task_cls
 class HTTPConnection(Task):
+    """HTTP Rest API for a scheduler runtime communication
+    using Flask. Useful for web UI or communication between
+    schedulers.
+    Not meant for heavy traffic.
+
+    Examples:
+        GET http://127.0.0.1:5000/system/ping
+        GET http://127.0.0.1:5000/tasks
+        GET http://127.0.0.1:5000/logs
+        GET http://127.0.0.1:5000/parameters
+
+        PUT http://127.0.0.1:5000/parameters
+        {"mode": "test"}
+    """
     # TODO: Token mechanism. A customizable method "is_authenticated(token, resource, method)" that
     # checks whether the token permits the action
 
@@ -65,3 +82,35 @@ class HTTPConnection(Task):
 
     def get_default_name(self):
         return "HTTP-API"
+
+
+class AddressPinger(Task):
+    
+    """Task for pushing the connection information
+    to a target host.
+
+    Useful if there is an HTTP interface for the scheduler 
+    and the scheduler is set on a dynamic IP or multiple 
+    schedulers should be automatically linked to a web UI
+    by utilizing the same configuration.
+
+    """
+    # TODO: Test
+    __status__ = "test"
+
+    def __init__(self, target_url, delay="5 minutes", connection_timeout=5, **kwargs):
+        super().__init__(**kwargs)
+        self.target_host = target_url
+        self.execution = "thread"
+        self.delay = pd.Timedelta(delay).total_seconds()
+        self.connection_timeout = connection_timeout
+
+    def execute_action(self):
+        while not self.thread_terminate.is_set():
+            ip = get_ip()
+            try:
+                requests.post(self.target_host, data={"node": platform.node(), "application": "Atlas Scheduler"}, timeout=self.connection_timeout)
+            except requests.ConnectionError:
+                pass
+            time.sleep(self.delay)
+
