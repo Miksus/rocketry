273c14d580990dbbd3b418d34ad0949c24baff24
Mikael Koli <koli.mikael@gmail.com>
2021-08-17 11:38:42 +0300

    docs: improved cond parsing documentations slightly.

--- powerbase/parse/_condition/condition_item.py
+++ powerbase/parse/_condition/condition_item.py
@@ -2,8 +2,7 @@
 
 
 import re
-
-
+from typing import Callable
 
 # TODO: How to distinquise between the actual task and dependency? Modify the set_default_task
 
@@ -25,21 +24,35 @@ class Parser:
         else:
             if s == self.expr:
                 return self.parser(s)
-        raise ParserError
+        raise ParserError(f"Could not parse string {repr(s)}.")
 
 CONDITION_PARSERS = []
 
-def add_condition_parser(s, func, regex=True):
+def add_condition_parser(s:str, func:Callable, regex:bool=True):
+    """Add a parsing instruction to be used for parsing a 
+    string to condition.
+
+    Parameters
+    ----------
+    s : str
+        Exact string (if regex=False) or regex (if regex=True)
+        to be matched. If regex and has groups, the groups are
+        passed to func.
+    func : Callable
+        Function that should return a condition. 
+    regex : bool, optional
+        Whether the 's' is a regex or exact string, 
+        by default True
+    """
     sentence = Parser(s, func, regex=regex)
     CONDITION_PARSERS.append(sentence)
 
 def parse_condition_item(s:str):
     "Parse one condition"
-    # TODO: Rename. Examples: parse_condition_item
     for parser in CONDITION_PARSERS:
         try:
             return parser(s)
         except ParserError:
             pass
-    raise ValueError(f"Cannot parse statement: {s}")
+    raise ValueError(f"Cannot parse the condition: {repr(s)}")
 
