ab2ff9a9980142b217753d47f5d2e728a47eb93d
Mikael Koli <koli.mikael@gmail.com>
2021-09-04 08:33:08 +0300

    docs: added doctest to sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'Mikael Koli'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc', 
     'sphinx.ext.coverage', 
     'sphinx.ext.napoleon'
