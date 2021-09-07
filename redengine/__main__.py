19d688ae3fb8c39cc92533785d82a01d585a27b7
Mikael Koli <koli.mikael@gmail.com>
2021-09-07 08:37:54 +0300

    cli: now has help in create and also changed sligtly the defaults.

--- redengine/__main__.py
+++ redengine/__main__.py
@@ -6,34 +6,33 @@ from redengine import Scheduler
 
 from distutils.dir_util import copy_tree
 
-# https://docs.python.org/3/library/argparse.html
 PKG_ROOT = Path(__file__).parent
+TEMPLATE_DIR = PKG_ROOT / "templates"
+
+TEMPLATE_OPTIONS = [folder.name for folder in TEMPLATE_DIR.glob("*/")]
 
 def start_project(target, template):
-    template_dir = PKG_ROOT / "templates"
-    source = template_dir / template
+    source = TEMPLATE_DIR / template
     if not source.is_dir():
-        tmpls = [folder.name for folder in template_dir.glob("*/")]
-        raise OSError(f"Template '{template}' does not exists. Full list: {tmpls}")
+        raise OSError(f"Template '{template}' does not exists. Full list: {TEMPLATE_OPTIONS}")
     copy_tree(str(source), str(target))
 
-def parse_args(args):
+def parse_args(args=None):
     parser = argparse.ArgumentParser(prog='Redengine', description="Scheduling framework.")
 
-    # See: https://docs.python.org/3/library/argparse.html#sub-commands
     subparsers = parser.add_subparsers(help='Command to use.', dest='command')
 
-    start_project_parser = subparsers.add_parser("create", parents=[], add_help=False, aliases=['quick-start'])
+    start_project_parser = subparsers.add_parser("create", parents=[], add_help=True, aliases=['quick-start'])
     
     start_project_parser.set_defaults(func=start_project)
 
-    start_project_parser.add_argument("target", default="scheduling-project", nargs='?', help='Name of the foder for the project.')
-    start_project_parser.add_argument("-t", "-template", default="minimal", dest="template", help='Template to use.', required=False)
+    start_project_parser.add_argument("target", default="engine-project", nargs='?', help='Name of the foder for the project.')
+    start_project_parser.add_argument("-t", "-template", default="minimal", dest="template", help=f'Template to generate. (Options: {TEMPLATE_OPTIONS})', required=False)
     
     return parser.parse_args(args)
 
 def main(args=None):
-    #main2()
+
     args = parse_args(args)
 
     args = vars(args)
