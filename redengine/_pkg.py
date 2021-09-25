9de7d408b6ab8bf16f8db6ade66eedce558478f6
Mikael Koli <koli.mikael@gmail.com>
2021-09-25 19:25:28 +0300

    fix: forgot a list check in raise_for_missing_imports

--- redengine/_pkg.py
+++ redengine/_pkg.py
@@ -3,4 +3,5 @@ import sys
 
 def raise_for_missing_imports(*args):
     missing = [pkg for pkg in args if pkg not in sys.modules]
-    raise ImportError(f"Missing packages: {missing}")
+    if missing:
+        raise ImportError(f"Missing packages: {missing}")
