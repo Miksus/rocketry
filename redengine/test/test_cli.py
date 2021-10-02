841e46771f7dc32ca3b248c883c8da93fe6ee769
Mikael Koli <koli.mikael@gmail.com>
2021-10-02 08:33:23 +0300

    test: refactored CLI tests

--- redengine/test/test_cli.py
+++ redengine/test/test_cli.py
@@ -1,16 +1,15 @@
 
-from redengine.conditions.scheduler import SchedulerCycles
-from redengine.cli import main
 from pathlib import Path
-
 import importlib
 import importlib.util
 import sys, os
 
-def load_module(mdl):
-    return importlib.import_module(mdl)
+import pytest
 
-def test_create(tmpdir):
+from redengine.conditions.scheduler import SchedulerCycles
+from redengine.cli import main
+
+def test_create_default(tmpdir):
     with tmpdir.as_cwd() as old_dir:
         assert not Path("myproject").is_dir()
 
@@ -19,30 +18,46 @@ def test_create(tmpdir):
         assert Path("myproject/tasks").is_dir()
         assert Path("myproject/main.py").is_file()
 
-def test_minimal(tmpdir):
-    with tmpdir.as_cwd() as old_dir:
-        assert not Path("myproject").is_dir()
+class CLIBase:
 
-        main(["create", "myproject", "--template", "minimal"])
+    template = None
+
+    @pytest.fixture#(scope="class")
+    def project(self, tmpdir):
+        with tmpdir.as_cwd() as old_dir:
+            main(["create", "myproject", "--template", self.template])
+            project_root = os.path.join(str(tmpdir), "myproject")
+            sys.path.append(project_root)
+            yield project_root
+            sys.path.remove(project_root)
+
+
+    def test_start(self, project):
+        module = importlib.import_module("main")
+
+        session = module.session
+        session.set_as_default()
         
-        try:
-            sys_path = os.path.join(str(tmpdir), "myproject")
-            sys.path.append(sys_path)
-            module = load_module("main")
+        # Make almost instant shutdown
+        session.scheduler.shut_cond = SchedulerCycles() == 1
+        session.start()
 
-            session = module.session
-            session.set_as_default()
-            
-            # Make almost instant shutdown
-            session.scheduler.shut_cond = SchedulerCycles() == 1
-            session.start()
+        assert 1 < len(session.tasks)
 
-            assert 1 < len(session.tasks)
+        # Check no tasks failed
+        assert ["success"] * len(session.tasks) == [
+            task.status
+            for task in session.tasks.values()
+        ]
 
-            # Check no tasks failed
-            assert ["success"] * len(session.tasks) == [
-                task.status
-                for task in session.tasks.values()
-            ]
-        finally:
-            sys.path.remove(sys_path)
+class TestStandalone(CLIBase):
+    template = "standalone"
+
+    def test_content(self, project):
+        assert os.listdir(project) == ["main.py"]
+
+class TestMinimal(CLIBase):
+    template = "minimal"
+
+class TestSimple(CLIBase):
+    template = "simple"
