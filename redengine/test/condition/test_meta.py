cc1eaa5cb66d0bad6f5b8e8b8b7512265966c090
Mikael Koli <koli.mikael@gmail.com>
2021-12-02 00:18:16 +0200

    test: updated task cond tests

--- redengine/test/condition/test_meta.py
+++ redengine/test/condition/test_meta.py
@@ -7,24 +7,20 @@ import re
 from redengine.core import Scheduler
 from redengine.tasks import FuncTask
 
-def is_foo(place):
-    print(f"evaluating: {place}")
-    if place == "home":
+def is_foo(status):
+    print(f"evaluating: {status}")
+    if status == "true":
         return True
     else:
         return False
 
 @pytest.mark.parametrize("execution", ["main", "thread", "process"])
-def test_taskcond(capsys, session, execution):
-    # Check if one check is enough
-    cond = TaskCond(syntax=re.compile(r"is foo at (?P<place>.+)"), start_cond="every 1 min", active_time="past 10 seconds", execution=execution)
+def test_taskcond_true(capsys, session, execution):
+
+    cond = TaskCond(syntax=re.compile(r"is foo (?P<status>.+)"), start_cond="every 1 min", active_time="past 10 seconds", execution=execution)
     cond(is_foo)
     
-    task = FuncTask(lambda: None, start_cond="is foo at home", name="a task", execution="main")
-    task_not_running = FuncTask(lambda: None, start_cond="is foo at work", name="task not running", execution="main")
-
-    # This condition should be cached (not creating third task)
-    FuncTask(lambda: None, start_cond="is foo at work", name="task not running 2", execution="main")
+    task = FuncTask(lambda: None, start_cond="is foo true", name="a task", execution="main")
 
     scheduler = Scheduler( # (TaskStarted(task="a task") >= 2) | 
         shut_cond=(TaskStarted(task="a task") >= 2) | ~SchedulerStarted(period="past 5 seconds")
@@ -45,22 +41,55 @@ def test_taskcond(capsys, session, execution):
         {"task_name": "a task", "action": "success"},
     ]
 
+    # Check cond task
+    cond_tasks = [task for task in session.tasks.values() if task.name.startswith("_condition")]
+    assert len(cond_tasks) == 1
+
+    cond_task = cond_tasks[0]
+    history_check = [
+        {"task_name": rec['task_name'], "action": rec["action"]} 
+        for rec in history
+        if rec['task_name'] == cond_task.name
+    ]
+    assert history_check == [
+        {"task_name": cond_task.name, "action": "run"},
+        {"task_name": cond_task.name, "action": "success"},
+    ] 
+
+@pytest.mark.parametrize("execution", ["main", "thread", "process"])
+def test_taskcond_false(capsys, session, execution):
+
+    cond = TaskCond(syntax=re.compile(r"is foo (?P<status>.+)"), start_cond="every 1 min", active_time="past 10 seconds", execution=execution)
+    cond(is_foo)
+    
+    task = FuncTask(lambda: None, start_cond="is foo false", name="a task", execution="main")
+
+    scheduler = Scheduler( # (TaskStarted(task="a task") >= 2) | 
+        shut_cond=SchedulerCycles() >= 3
+    )
+
+    scheduler()
+    
+    history = list(session.get_task_log())
     history_task = [
         {"task_name": rec['task_name'], "action": rec["action"]} 
         for rec in history
-        if rec['task_name'] == "task not running"
+        if rec['task_name'] == "a task"
     ]
     assert history_task == []
 
+    # Check cond task
     cond_tasks = [task for task in session.tasks.values() if task.name.startswith("_condition")]
-    assert len(cond_tasks) == 2
-    for cond_task in cond_tasks:
-        history_check = [
-            {"task_name": rec['task_name'], "action": rec["action"]} 
-            for rec in history
-            if rec['task_name'] == cond_task.name
-        ]
-        assert history_check == [
-            {"task_name": cond_task.name, "action": "run"},
-            {"task_name": cond_task.name, "action": "success"},
-        ] 
+    assert len(cond_tasks) == 1
+    
+    cond_task = cond_tasks[0]
+    history_check = [
+        {"task_name": rec['task_name'], "action": rec["action"]} 
+        for rec in history
+        if rec['task_name'] == cond_task.name
+    ]
+    assert history_check == [
+        {"task_name": cond_task.name, "action": "run"},
+        {"task_name": cond_task.name, "action": "success"},
+    ] 
+
