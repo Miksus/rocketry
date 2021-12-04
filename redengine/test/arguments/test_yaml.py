0ca19bc756c8bdfb2b48031089e13f99e54640d9
Mikael Koli <koli.mikael@gmail.com>
2021-12-04 20:20:33 +0200

    upd: YamlArg Renamed YamlArg items to field. Also added a test.

---
+++ redengine/test/arguments/test_yaml.py
@@ -0,0 +1,15 @@
+
+from textwrap import dedent
+import pytest
+from redengine.arguments import YamlArg
+
+def test_construct(tmpdir):
+    pytest.importorskip("yaml")
+    fh = tmpdir.join("arg.yaml")
+    fh.write(dedent("""
+    myfield:
+        innerfield: 'a value'
+    """))
+    file = str(fh)
+    arg = YamlArg(file, field=['myfield', 'innerfield'])
+    assert 'a value' == arg.get_value()
