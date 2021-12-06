3aa74487a8b3f71aabe7b6ebcec4ec3afb90a1cd
Mikael Koli <koli.mikael@gmail.com>
2021-12-06 11:45:04 +0200

    docs: add type hints

--- redengine/_base.py
+++ redengine/_base.py
@@ -1,5 +1,9 @@
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from redengine import Session
 
 class RedBase:
     """Baseclass for all Red Engine classes"""
-    session = None
+    session: 'Session' = None
