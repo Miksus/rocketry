f48aacddcad129accd574f782bf498731386ce3a
Mikael Koli <koli.mikael@gmail.com>
2022-06-28 07:20:44 +0300

    fix: import error on logging parser

--- redengine/parse/session.py
+++ redengine/parse/session.py
@@ -1,9 +1,8 @@
 
 from . import StaticParser, Field
-from . import parse_logging, parse_scheduler, parse_tasks, parse_session_params
+from . import parse_scheduler, parse_tasks, parse_session_params
 
 session_parser = StaticParser({
-        "logging": Field(parse_logging, if_missing="ignore"),
         "parameters": Field(parse_session_params, if_missing="ignore", types=(dict,)),
         "tasks": Field(parse_tasks, if_missing="ignore", types=(dict, list)),
         "scheduler": Field(parse_scheduler, if_missing="ignore"),
