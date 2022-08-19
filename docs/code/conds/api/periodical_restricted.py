c72d8d2e96a024baa04c86d17f6d516846a0454b
Mikael Koli <koli.mikael@gmail.com>
2022-08-19 10:53:22 +0300

    docs: updated code snippet

--- docs/code/conds/api/periodical_restricted.py
+++ docs/code/conds/api/periodical_restricted.py
@@ -1,25 +1,25 @@
 from rocketry.conds import minutely, hourly, daily, weekly, monthly
 
 @app.task(minutely.before("45"))
-def do_minutely():
+def do_before():
     ...
 
 @app.task(hourly.after("45:00"))
-def do_hourly():
+def do_after():
     ...
 
 @app.task(daily.between("08:00", "14:00"))
-def do_daily():
+def do_between():
     ...
 
 @app.task(daily.at("11:00"))
-def do_weekly():
+def do_at():
     ...
 
 @app.task(weekly.on("Monday"))
-def do_weekly():
+def do_on():
     ...
 
 @app.task(monthly.starting("3rd"))
-def do_monthly():
+def do_starting():
     ...
