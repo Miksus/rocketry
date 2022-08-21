d4b8e14df9fcc48bbb49e86182762dc18acedfc8
Mikael Koli <koli.mikael@gmail.com>
2022-08-21 20:59:58 +0300

    docs: added docstrings to application components

--- rocketry/application.py
+++ rocketry/application.py
@@ -22,15 +22,66 @@ class _AppMixin:
     session: Session
 
     def task(self, start_cond=None, name=None, **kwargs):
-        "Create a task"
+        """Create a task
+        
+        Parameters
+        ----------
+        start_cond : BaseCondition
+            Starting condition of the task. When evaluates
+            True, the task is set to run.
+        name : str
+            Name of the task. If not specified, the name
+            is derived by the task class. For function
+            tasks it is the name of the function.
+        **kwargs
+            Additional arguments passed to the task class.
+
+        Examples
+        --------
+        .. codeblock::
+
+            @app.task("daily")
+            def get_value():
+                return "Hello"
+        """
         return self.session.create_task(start_cond=start_cond, name=name, **kwargs)
 
     def param(self, name:Optional[str]=None):
-        "Set one session parameter (decorator)"
+        """Create a parameter (from function)
+        
+        Parameters
+        ----------
+        name : str
+            Name of the parameter.
+
+        Examples
+        --------
+        .. codeblock::
+
+            @app.param("my_value")
+            def get_value():
+                return "Hello"
+        """
         return FuncParam(name, session=self.session)
 
     def cond(self, syntax: Union[str, Pattern, List[Union[str, Pattern]]]=None):
-        "Create a condition (decorator)"
+        """Create a custom condition (from function)
+        
+        Parameters
+        ----------
+        syntax : str, regex pattern, optional
+            String expression for the condition. Used
+            for condition language.
+
+        Examples
+        --------
+        .. codeblock::
+
+            @app.cond()
+            def is_foo():
+                return True or False
+
+        """
         return FuncCond(syntax=syntax, session=self.session, decor_return_func=False)
 
     def params(self, **kwargs):
@@ -38,6 +89,7 @@ class _AppMixin:
         self.session.parameters.update(kwargs)
 
     def include_grouper(self, group:'Grouper'):
+        """Include contents of a grouper to the application"""
         for task in group.session.tasks:
             if group.prefix:
                 task.name = group.prefix + task.name
@@ -49,7 +101,18 @@ class _AppMixin:
         self.session.parameters.update(group.session.parameters)
 
 class Rocketry(_AppMixin):
-    """Rocketry scheduling application"""
+    """Rocketry scheduling application
+    
+    Parameters
+    ----------
+    session : Session, optional
+        Scheduling session. Created if not passed.
+    logger_repo: redbird.base.BaseRepo, optional
+        Repository for the log records. MemoryRepo
+        is created if not passed.
+    **kwargs
+        Keyword arguments passed to session creation.
+    """
 
     def __init__(self, session:Session=None, logger_repo:Optional[BaseRepo]=None, execution=None, **kwargs):
 
@@ -70,7 +133,7 @@ class Rocketry(_AppMixin):
         self.session.start()
 
     async def serve(self, debug=False):
-        "Run the scheduler"
+        "Async run the scheduler"
         self.session.config.debug = debug
         self.session.set_as_default()
         await self.session.serve()
@@ -106,6 +169,26 @@ class Rocketry(_AppMixin):
             raise NotImplementedError(f"Repo creation for {repo} not implemented")
 
 class Grouper(_AppMixin):
+    """Task group
+    
+    This is a group of tasks (and other components)
+    that can be added to an application later. Useful
+    for bigger applications.
+
+    Parameters
+    ----------
+    prefix : str, optional
+        Prefix to add in front of the task names
+        when including the grouper to an app.
+    start_cond : BaseCondition, optional
+        Condition that is added to every tasks' start
+        condition in the group when including to an app.
+        Every task in the group must fulfill this 
+        condition as well to start.
+    execution : str, optional
+        Execution of the tasks in the group if not specified
+        in the task. 
+    """
 
     def __init__(self, prefix:str=None, start_cond=None, execution=None):
         self.prefix = prefix
