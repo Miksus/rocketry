e72cd6cb0bf2e903b878b2bbfb507122041fa438
Mikael Koli <koli.mikael@gmail.com>
2022-09-05 21:59:27 +0300

    test: fix starting cond tests

--- rocketry/test/condition/test_parse.py
+++ rocketry/test/condition/test_parse.py
@@ -49,10 +49,10 @@ cases_time = [
 
     pytest.param("secondly starting 500",   TaskExecutable(period=TimeOfSecond.starting("500")), id="secondly starting"),
     pytest.param("minutely starting 45:00",   TaskExecutable(period=TimeOfMinute.starting("45:00")), id="minutely starting"),
-    pytest.param("hourly starting 45:00",   TaskExecutable(period=TimeOfHour("45:00", "45:00")), id="hourly starting"),
-    pytest.param("daily starting 10:00",    TaskExecutable(period=TimeOfDay("10:00", "10:00")),  id="daily starting"),
-    pytest.param("weekly starting Tuesday", TaskExecutable(period=TimeOfWeek("Tue", "Tue")),     id="weekly starting"),
-    pytest.param("monthly starting 1.",  TaskExecutable(period=TimeOfMonth("1.", "1.")), id="monthly starting"),
+    pytest.param("hourly starting 45:00",   TaskExecutable(period=TimeOfHour.starting("45:00")), id="hourly starting"),
+    pytest.param("daily starting 10:00",    TaskExecutable(period=TimeOfDay.starting("10:00")),  id="daily starting"),
+    pytest.param("weekly starting Tuesday", TaskExecutable(period=TimeOfWeek.starting("Tue")),     id="weekly starting"),
+    pytest.param("monthly starting 1.",  TaskExecutable(period=TimeOfMonth.starting("1.")), id="monthly starting"),
 
     pytest.param("hourly between 45:00 and 50:00",       TaskExecutable(period=TimeOfHour("45:00", "50:00")), id="hourly between"),
     pytest.param("daily between 10:00 and 14:00",        TaskExecutable(period=TimeOfDay("10:00", "14:00")),  id="daily between"),
