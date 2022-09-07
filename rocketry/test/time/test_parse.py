0935a07914310c4c47ad17335bc83e582c8b3f4f
Mikael Koli <koli.mikael@gmail.com>
2022-09-07 08:37:44 +0300

    test: add more tests for coverage

--- rocketry/test/time/test_parse.py
+++ rocketry/test/time/test_parse.py
@@ -1,11 +1,15 @@
 
-from rocketry.parse import parse_time
-from rocketry.time import TimeOfDay, TimeOfHour, TimeOfMinute, TimeDelta, TimeOfWeek, TimeOfYear, TimeOfMonth
+from rocketry.parse import parse_time, ParserError
+from rocketry.time import TimeOfDay, TimeOfHour, TimeOfMinute, TimeDelta, TimeOfWeek, TimeOfYear, TimeOfMonth, always, never
+
 
 import pytest
 
 @pytest.mark.parametrize(
     "time_str,expected", [
+        pytest.param("always", always, id="always"),
+        pytest.param("never", never, id="never"),
+
         pytest.param("every 10 seconds", TimeDelta("10 seconds"), id="every"),
 
         pytest.param("time of minute between 15:00 and 30:00", TimeOfMinute("15:00", "30:00"), id="TimeOfMinute between"),
@@ -24,8 +28,15 @@ import pytest
 
         pytest.param("time of month between 1st and 2nd", TimeOfMonth("1st", "2nd"), id="TimeOfMonth between (1st, 2nd)"),
         pytest.param("time of month between 3rd and 4th", TimeOfMonth("3rd", "4th"), id="TimeOfMonth between (3rd, 4th)"),
+
+        pytest.param("time of week on Monday | time of week on Friday", TimeOfWeek.at("Mon") | TimeOfWeek.at("Fri"), id="OR"),
+        pytest.param("time of week on Monday & time of day between 10:00 and 12:00", TimeOfDay("10:00", "12:00") & TimeOfWeek.at("Mon"), id="AND"),
     ]
 )
 def test_string(time_str, expected):
     cond = parse_time(time_str)
     assert cond == expected
+
+def test_error():
+    with pytest.raises(ParserError):
+        cond = parse_time("invalid string")
