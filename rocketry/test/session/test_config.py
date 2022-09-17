7fc52403859190f81e693db3b391bd623e7a094d
Mikael Koli <koli.mikael@gmail.com>
2022-09-17 22:01:51 +0300

    test: fix session warning

--- rocketry/test/session/test_config.py
+++ rocketry/test/session/test_config.py
@@ -1,6 +1,6 @@
 from rocketry import Session
 
 def test_defaults():
-    s = Session()
+    s = Session(config={'task_execution': 'async'})
     config = s.config
     assert config.cycle_sleep == 0.1
