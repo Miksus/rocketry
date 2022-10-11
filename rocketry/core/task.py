17fe2422b22273a4443b49e498aff8b7f44b2aea
Mikael Koli <koli.mikael@gmail.com>
2022-10-11 22:15:54 +0300

    rem: removed not needed method

--- rocketry/core/task.py
+++ rocketry/core/task.py
@@ -535,9 +535,6 @@ class Task(RedBase, BaseModel):
     def __bool__(self):
         return self.is_runnable()
 
-    def is_terminable(self):
-        return self.end_cond.observe(task=self)
-
     def is_runnable(self):
         """Check whether the task can be run or not.
 
