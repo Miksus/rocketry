1b19ad5e6baa45aea3bb86641db695c51252d426
Mikael Koli <koli.mikael@gmail.com>
2022-10-29 11:35:48 +0300

    upd: events now use repos

--- rocketry/events.py
+++ rocketry/events.py
@@ -3,21 +3,34 @@ import datetime
 import time
 from typing import Any, Dict, List, Optional, Set
 import inspect
+from uuid import uuid4
 
-from pydantic import BaseModel, Field
+from redbird import BaseRepo
+from redbird.repos import MemoryRepo
+from redbird.oper import less_equal, less_than
+from pydantic import BaseModel, Field, validator
 
 from rocketry.args import Task as TaskArg
 from rocketry.conditions import BaseCondition
 from rocketry.core import BaseArgument, Task
 from rocketry.conds import new_scheduler_cycle
+from rocketry.pybox.time.convert import to_timestamp
 
 class BaseEvent(ABC, BaseModel):
-    id: Optional[str]
-    timestamp: datetime.datetime = Field(default_factory=datetime.datetime.now)
+    __only_latest__:bool = False
+
+    id: str = Field(default_factory=uuid4)
+    timestamp: float = Field(default_factory=time.time)
     value: Any = None
     
     def __init__(self, value, **kwargs):
         super().__init__(value=value, **kwargs)
+
+    @validator("timestamp", pre=True)
+    def parse_timestamp(cls, value):
+        if isinstance(value, datetime.datetime):
+            return value.timestamp()
+        return value
 
     @abstractmethod
     def consume(self, task):
@@ -45,13 +58,13 @@ class Batch(BaseEvent):
 
 class Event(BaseEvent):
     "Basic event"
-
+    __only_latest__:bool = True
     def consume(self, task):
         ...
 
     def triggered(self, task):
         last_run = task.get_last_run()
-        in_future = self.timestamp > datetime.datetime.now()
+        in_future = self.timestamp > time.time()
         if in_future:
             # Future events won't trigger task
             return False
@@ -81,9 +94,9 @@ class EventStream(BaseCondition, BaseArgument):
         1. Get items returned by the function
         2. 
     """
-    event_stack: List[BaseEvent]
+    event_stack: BaseRepo
 
-    def __init__(self, check_cond:BaseCondition=None, model:BaseEvent=Event, timeout=None):
+    def __init__(self, check_cond:BaseCondition=None, repo:BaseRepo=None, model:BaseEvent=Event, max_size=None, timeout=None):
         # How:
         # - Delete previous, add new (maintain consumed)
         # - Accumulate everything (can delete consumed)
@@ -93,10 +106,14 @@ class EventStream(BaseCondition, BaseArgument):
             check_cond = new_scheduler_cycle()
         self.check_cond = check_cond
         self.model = model
+        
         self.timeout = timeout
+        self.max_size = max_size
+
         self._last_check = None
-        self.event_stack = []
-        self._processed_ids = set()
+        self.event_stack = MemoryRepo(id_field="id") if repo is None else repo
+
+        self.event_stack.model = model
 
     def decorate(self, func):
         self.func = func
@@ -111,14 +128,13 @@ class EventStream(BaseCondition, BaseArgument):
         if item:
             if task is not None:
                 item.consume(task)
-                if item.id is not None:
-                    self._processed_ids.add(item.id)
+                self.event_stack.update(item)
             return item.value
 
     def get_next(self, task, **kwargs):
         items = self._get_items(task=task, **kwargs)
         for item in items:
-            is_triggered = item.triggered(task) and item.id not in self._processed_ids
+            is_triggered = item.triggered(task)
             if is_triggered:
                 return item
 
@@ -135,15 +151,30 @@ class EventStream(BaseCondition, BaseArgument):
                     item = self._parse_event(item)
                     # TODO: This is run multiple times, same items are appended several times
                     # TODO: Check if item.id in item stack
-                    self.event_stack.append(item)
+                    self.event_stack.add(item)
+            
             # Remove timeouted
-            self.event_stack = [
-                item
-                for i, item in enumerate(self.event_stack)
-                if not item.inactive(position=i, stream=self)
-            ]
+            self._clean()
             self._last_check = time.time()
         return self.event_stack
+
+    def _clean(self):
+        only_latest = self.model.__only_latest__
+        if only_latest:
+            latest = self.event_stack.filter_by(timestamp=less_equal(time.time())).last()
+            if latest:
+                self.event_stack.filter_by(timestamp=less_than(latest.timestamp)).delete()
+
+        if self.max_size is not None or self.timeout is not None:
+            n_events = self.event_stack.filter_by().count()
+            n_delete = n_events - self.max_size if self.max_size is not None else 0
+            now = time.time()
+            for i, item in enumerate(self.event_stack):
+                if n_delete > 0:
+                    self.event_stack.remove(item)
+                    n_delete -= 1
+                elif self.timeout is not None and now - to_timestamp(item.timestamp) > self.timeout:
+                    self.event_stack.remove(item)
 
     def _is_timeouted(self, item):
         if hasattr(item, "timestamp"):
@@ -152,6 +183,9 @@ class EventStream(BaseCondition, BaseArgument):
                 return True
         return False
 
+    def __bool__(self):
+        raise NotImplementedError
+
     def _parse_event(self, event) -> BaseEvent:
         if isinstance(event, datetime.datetime):
             return self.model(timestamp=event, value=event)
@@ -159,6 +193,9 @@ class EventStream(BaseCondition, BaseArgument):
             return event
         return self.model(timestamp=datetime.datetime.now(), value=event)
 
+    @property
+    def session(self):
+        raise AttributeError
 
 def event_stream(*args, **kwargs):
     "Stream of events"
