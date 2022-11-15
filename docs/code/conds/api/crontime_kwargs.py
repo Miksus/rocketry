3edfede5214bc888a0f96368ca0bc487bc99de6b
Mark Mayo <mark@there.co.nz>
2022-11-16 12:27:37 +1300

    Update crontime_kwargs.py
    reintroduce ellipsis

--- docs/code/conds/api/crontime_kwargs.py
+++ docs/code/conds/api/crontime_kwargs.py
@@ -3,6 +3,7 @@ from rocketry.conds import crontime
 @app.task(crontime(minute="*/5"))
 def do_simple():
     "Run at every 5th minute"
+    ...
 
 
 @app.task(crontime(minute="*/2", hour="7-18", day_of_month="1,2,3", month="Feb-Aug/2"))
