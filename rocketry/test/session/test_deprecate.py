cf8bbbf24f9e0cb1f410bd365281e8c11a6f7912
Mikael Koli <koli.mikael@gmail.com>
2022-11-23 21:51:39 +0200

    test: removed test for FutureWarning

--- rocketry/test/session/test_deprecate.py
+++ rocketry/test/session/test_deprecate.py
@@ -8,17 +8,3 @@ def test_shutdown(session):
     with pytest.warns(DeprecationWarning):
         session.shutdown()
     assert session.scheduler._flag_shutdown.is_set()
-
-def test_no_execution_method():
-
-    with pytest.warns(FutureWarning):
-        Session()
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("error")
-
-        # Test the following won't warn
-        Session(config=dict(execution="process"))
-        Session(config=dict(execution="thread"))
-        Session(config=dict(execution="main"))
-        Session(config=dict(execution="async"))
