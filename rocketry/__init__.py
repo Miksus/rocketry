089ba457828280a6cf402eeb47f0448eadebc8a0
Mikael Koli <koli.mikael@gmail.com>
2022-11-26 00:50:18 +0200

    test: add version test to verify build is tested

--- rocketry/__init__.py
+++ rocketry/__init__.py
@@ -1,7 +1,13 @@
 from .session import Session
 from .application import Rocketry, Grouper
-from ._version import *
 from .core import Scheduler
+
+try:
+    from ._version import *
+except ImportError:
+    # Package was not built the standard way
+    __version__ = version = '0.0.0.UNKNOWN'
+    __version_tuple__ = version_tuple = (0, 0, 0, 'UNKNOWN', '')
 
 from ._setup import _setup_defaults
 from . import (
