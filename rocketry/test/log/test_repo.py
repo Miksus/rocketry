2c0e188cd589109abfeff3b165e569969c0e4601
Mikael Koli <koli.mikael@gmail.com>
2022-12-01 18:08:29 +0200

    test: skip SQL test if sqlalchemy not installed

--- rocketry/test/log/test_repo.py
+++ rocketry/test/log/test_repo.py
@@ -12,13 +12,14 @@ def get_csv(model, tmpdir):
     return CSVFileRepo(filename=str(file), model=model)
 
 def get_sql(model, tmpdir):
+    pytest.importorskip("sqlalchemy")
     return SQLRepo(conn_string="sqlite://", table="mylogs", if_missing="create", model=model, id_field="created")
 
 @pytest.mark.parametrize("get_repo", [get_csv, get_sql])
 @pytest.mark.parametrize("model", [MinimalRecord, MinimalRunRecord, TaskLogRecord, TaskRunRecord])
 def test_cache(session, tmpdir, model, get_repo):
     if get_repo == get_sql and model in (TaskRunRecord, TaskLogRecord) and redbird.version_tuple[:3] <= (0, 6, 0):
-        pytest.skip(reason="Red Bird <=0.6.0 does not support date-like in SQL table creation")
+        pytest.skip(reason="Red Bird <= 0.6.0 does not support date-like in SQL table creation")
     repo = get_repo(model=model, tmpdir=tmpdir)
     task_logger = logging.getLogger(session.config.task_logger_basename)
     task_logger.handlers = [
